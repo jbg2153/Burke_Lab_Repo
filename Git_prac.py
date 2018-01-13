9ec4bf576697abd6afe4fdc018166fc8ebc93fab
Unknown <jbg2153@columbia.edu>
2018-01-13 09:12:23 -0500

    Add Git_prac.py
    Added practice file to the repo

---
+++ Git_prac.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 13 09:07:18 2018
+
+@author: justin
+
+Whatever you want to this just to learn the basics of how git works. Make sure
+to tag it with your name somehow.
+"""
+
+print('Justin says, "Welcome to GIT."')
